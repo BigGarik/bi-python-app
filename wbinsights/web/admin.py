82edcc4f71ae00eb7d2acd11d8fc26bbfbe7fafa
Igor Bocharov <inetsmol@gmail.com>
2024-02-22 09:31:17 +0500

    отображение статей

--- wbinsights/web/admin.py
+++ wbinsights/web/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Article, Category
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'slug', 'is_published', 'cat',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Article, ArticleAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
--- wbinsights/web/urls.py
+++ wbinsights/web/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from . import views
+from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("articles/", ArticleListView.as_view(), name='article_list'),
+    path('articles/<slug:slug>', ArticleDetailView.as_view(), name='article_detail'),
 ]
