d23cde442d53fc14681a9f81217f2b3c0968200e
Igor Bocharov <inetsmol@gmail.com>
2024-02-26 11:45:06 +0500

    ResearchesListView в urls

--- wbinsights/web/urls.py
+++ wbinsights/web/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
+from .views import ResearchesListView
 from .views.index import handleIndex
-from .views.login import  SignUpView
+from .views.login import SignUpView
 from .views.articles import ArticleDetailView, ArticleListView
 
 urlpatterns = [
     path("", handleIndex, name="index"),
     path("articles/", ArticleListView.as_view(), name='article_list'),
     path('articles/<slug:slug>', ArticleDetailView.as_view(), name='article_detail'),
-    path("researches/", ArticleListView.as_view(), name='research_list'),
+    path("researches/", ResearchesListView.as_view(), name='research_list'),
     path("signup/", SignUpView.as_view(), name="signup"),
 ]
