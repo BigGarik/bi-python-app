138e48343f980818564b7a17163b05f12f095d4b
Demerchyan Eduard <eduard.demerchyan@gmail.com>
2024-03-01 14:51:25 +0500

    add restrat gunicorn

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-source venv/bin/activate
 pip install -r requirements.txt          
 python ./wbinsights/manage.py collectstatic --noinput
 python ./wbinsights/manage.py migrate
+service gunicorn restart
