4ac337aa115859bc6dded056a41abf9e143ebb8c
Demerchyan Eduard <eduard.demerchyan@gmail.com>
2024-03-14 16:09:53 +0500

    Add links for left menu

--- wbinsights/web/views/login.py
+++ wbinsights/web/views/login.py
@@ -3,86 +3,103 @@ from django.contrib.auth.views import LoginView
 from django.contrib import messages
 from django.http import HttpResponse
 
-
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
+from django.shortcuts import render
 
 from django import forms
 
 from web.models import CustomUser, Profile
+from web.models.users import ExpertProfile
 
 
+# class CustomUserChangeForm(UserChangeForm):
+#     class Meta:
+#         model = CustomUser
+#         fields = ("username", "email")
 
-
-
-
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = CustomUser
-        fields = ("username", "email")
-
-
-
-#Кастомный класс обработчик авторизации
-class WBILoginView(LoginView):
-
-    redirect_authenticated_user = True
-
-    def get_success_url(self):
-        return reverse_lazy('index')
-
-    def form_invalid(self, form):
-        messages.error(self.request, 'Неправильный логин или пароль')
-        return self.render_to_response(self.get_context_data(form=form))
-
-    #template_name = "users/login.html"
-    
-    pass
-
-#Форма регистрации пользователя
+# Форма регистрации пользователя
 class CustomUserCreationForm(UserCreationForm):
+    user_type = forms.ChoiceField(choices=Profile.TypeUser, widget=forms.RadioSelect(attrs={'class': '4444'}))
 
-    
-    # username = forms.CharField(label="ФИО", widget=forms.TextInput(attrs={'class': '123'}))
-    # email    = forms.CharField(label="емаил", widget=forms.EmailInput(attrs={'class': '123'}))
+    first_name = forms.CharField(label="Имя", widget=forms.TextInput(attrs={'class': '123'}))
+    last_name = forms.CharField(label="Фамилия", widget=forms.TextInput(attrs={'class': '123'}))
+    email = forms.CharField(label="Email", widget=forms.EmailInput(attrs={'class': '123'}))
     # phone_number = forms.RegexField(label="телефон", widget=forms.TextInput(attrs={'class': '123'}), 
     #                                 regex="^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$")
-    # password1 = forms.CharField(label="p1", widget=forms.PasswordInput(attrs={'class': '123'}))
-    # password2 = forms.CharField(label="p2", widget=forms.PasswordInput(attrs={'class': '123'}))
+    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': '123'}))
+    password2 = forms.CharField(label="Повторите пароль", widget=forms.PasswordInput(attrs={'class': '123'}))
 
-    user_type = forms.ChoiceField(choices= Profile.TypeUser, widget=forms.RadioSelect(attrs={'class':'4444'})) 
-       
-    #phone_number = forms.RegexField(regex="^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$")
-    def save(self):
-        print("123")
-        #instance = super(CustomUserCreationForm, self).save()
+    # phone_number = forms.RegexField(regex="^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$")
 
     class Meta:
         model = CustomUser
-        fields = ("first_name", "last_name", "email")
-        #fields = ("user_type", "username", "email", "phone_number", "password1", "password2")
+        fields = ("first_name", "last_name", "email", "password1", "password2")
+        # fields = ("user_type", "username", "email", "phone_number", "password1", "password2")
+
+
+class ExpertProfileForm(forms.ModelForm):
+    class Meta:
+        model = ExpertProfile
+        fields = ("about", "experience", "hour_cost")
+
+
+def register_user(request):
+    if request.method == 'POST':
+
+        user_form = CustomUserCreationForm(request.POST)
+
+        if user_form.is_valid():
+
+            if user_form.cleaned_data["user_type"] and user_form.cleaned_data["user_type"] == '1':
+
+                expert_profile_form = ExpertProfileForm(request.POST)
+
+                if expert_profile_form.is_valid():
+                    new_user = user_form.save()
+
+                    new_user_profile = Profile()
+                    new_user_profile.user = new_user
+                    new_user_profile.type = Profile.TypeUser.EXPERT
+                    new_user_profile.save()
+
+                    new_expert_profile = expert_profile_form.save(commit=False)
+                    new_expert_profile.user = new_user
+                    new_expert_profile.save()
+
+            else:
+                new_user = user_form.save()
+
+                new_user_profile = Profile()
+                new_user_profile.user = new_user
+                new_user_profile.type = Profile.TypeUser.USER
+                new_user_profile.save()
+
+    context = {
+        "user_form": CustomUserCreationForm(),
+        "expert_form": ExpertProfileForm()
+    }
+
+    return render(request, "registration/signup.html", context=context)
 
 
 class WBIRegisterUser(CreateView):
+    # create CustomUser
 
-    #create CustomUser
+    # if (user_type == 'Expert')
 
-    #if (user_type == 'Expert')
+    # we have to Create ExpertProfile
+    # send email
 
-    #we have to Create ExpertProfile
-    #send email
+    # else
 
-    #else
+    # send activation link
 
-    #send activation link
-    
+    # def form
 
-   # def form
-    
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
-
 
     # def form_valid(self, form: CustomUserCreationForm) -> HttpResponse:
 
