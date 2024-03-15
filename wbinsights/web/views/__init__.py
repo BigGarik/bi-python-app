a2d003a6c176bcead4ad9d4ec8080fdad00aefc8
Demerchyan Eduard <eduard.demerchyan@gmail.com>
2024-03-15 11:02:09 +0500

    fixed wrong commit errors

--- wbinsights/web/views/__init__.py
+++ wbinsights/web/views/__init__.py
@@ -1,5 +1,5 @@
 from .articles import ArticleListView, ArticleDetailView
-from .login import CustomUserCreationForm, CustomUserChangeForm, WBIRegisterUser
+from .login import CustomUserCreationForm, WBIRegisterUser
 from .index import handleIndex, page_not_found
 from .researches import ResearchesListView, ResearchesDetailView
 from .experts import ExpertListView, ExpertDetailView
