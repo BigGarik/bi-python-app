d688039eeac447734d84490934099ab4ccd40166
Demerchyan Eduard <eduard.demerchyan@gmail.com>
2024-04-04 15:28:42 +0500

    add compilemessages command

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt          
 python ./wbinsights/manage.py collectstatic --noinput
 python ./wbinsights/manage.py migrate
+python ./wbinsights/manage.py compilemessages
 service gunicorn restart
