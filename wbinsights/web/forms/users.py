72b7e1c31db31784131bbb3de8020c6546d64c36
Shawn Pantzlaff <2001shawnp@gmail.com>
2024-05-22 15:46:27 +0300

    tab changes

--- wbinsights/web/forms/users.py
+++ wbinsights/web/forms/users.py
@@ -35,7 +35,7 @@ class CustomUserCreationForm(UserCreationForm):
     password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput(attrs={'class': 'form-inputs-custom'}))
     password2 = forms.CharField(label=_("Confirm password"),
                                 widget=forms.PasswordInput(attrs={'class': 'form-inputs-custom'}))
-    # captcha = ReCaptchaField(label=_("Captcha"))
+    captcha = ReCaptchaField(label=_("Captcha"))
 
     class Meta:
         model = CustomUser
