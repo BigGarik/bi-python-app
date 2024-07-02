cb107a18bef6ae0f7fb977c2e12ae5ea0d06bbaa
Demerchyan Eduard <eduard.demerchyan@gmail.com>
2024-07-02 17:04:18 +0500

    Fixed categories save while register user

--- wbinsights/web/views/login.py
+++ wbinsights/web/views/login.py
@@ -91,7 +91,7 @@ def register_user(request):
 
             if user_form.is_valid() and expert_profile_form.is_valid():
                 new_user = save_new_user_and_profile(request, user_form, Profile.TypeUser.EXPERT)
-                new_expert_profile = expert_profile_form.save(commit=False)
+                new_expert_profile = expert_profile_form.save()
                 new_expert_profile.user = new_user
                 new_expert_profile.save()
                 return redirect('signup_success')
