c92c09d58781a00a123586c31d31bc236d042bd6
Shawn Pantzlaff <2001shawnp@gmail.com>
2024-11-22 08:04:25 +0300

    self commit refactoring and plugin

--- wbinsights/web/views/researches.py
+++ wbinsights/web/views/researches.py
@@ -3,14 +3,14 @@ from django.views.generic import ListView, DetailView
 
 from web.views.experts import ExpertListView
 from web.models import Research
-from web.utils import  is_mobile
+from web.utils import is_mobile_by_request
 import re
 
 
 class DeviceDetectionView(View):
     def get(self, request, *args, **kwargs):
 
-        if is_mobile():
+        if is_mobile_by_request(request):
             return ExpertListView.as_view()(request, *args, **kwargs)
         else:
             return ResearchesListView.as_view()(request, *args, **kwargs, extra_context={'is_mobile': False})
